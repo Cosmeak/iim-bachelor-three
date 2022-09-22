a358f6ebe5d6d6ae48ef539196df184931fed802
Cosmeak <guillaumefine09@gmail.com>
2022-09-22 14:28:35 +0200

    :sparkles: FEAT booking routes

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // Import controllers
 const userController = require('./src/controllers/userController')
+const bookingController = require('./src/controllers/bookingController')
 
 // Time logger
 router.use(function timeLog (req, res, next) {
@@ -17,7 +18,17 @@ router.get('/', (req, res, next) => {
     })
 })
 
+// Users routes
 router.route('/users')
     .get(userController.index)
+
+// Booking routes
+router.route('/booking')
+    .get(bookingController.index)
+    .post(bookingController.store)
+
+router.route('/booking/:id')
+    .get(bookingController.show)
+    .delete(bookingController.destroy)
 
 module.exports = router
