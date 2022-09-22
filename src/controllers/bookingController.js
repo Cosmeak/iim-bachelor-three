93aebecde0bc4888198955b4b1e60be59d594041
Cosmeak <guillaumefine09@gmail.com>
2022-09-22 22:55:28 +0200

    :construction: index return tentative

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,21 +1,25 @@
+const moment = require('moment')
 const booking = require('../models/booking')
 
+// Set nb of seat/table available
+const table = 24
+const seat = table * 2
+
 exports.index = (req, res, next) => {
-    booking.find()
-        .then(docs => {
-            return res.status(200)
-                .json({
-                    status: 'Succes',
-                    data: docs
-                })
-        })
-        .catch(err => {
-            return res.status(500)
-                .json({
-                    status: 'Failure',
-                    error: err
-                })
-        })
+    const query = req.query
+    const gte = query.gte ?? moment().startOf('day').toDate()
+    const lte = query.lte ?? moment(gte).endOf('day').toDate()
+
+    booking.find({
+        date: {
+            $gte: gte,
+            $lte: lte
+        }
+    })
+    .then(docs => {
+        console.log(docs, gte, lte)
+        return docs
+    })
 }
 
 exports.show = (req, res, next) => {
@@ -41,6 +45,7 @@ exports.store = (req, res, next) => {
         name: req.body.name,
         email: req.body.email ?? null,
         phone: req.body.phone ?? null,
+        nb_people: req.body.nb_people,
         date: req.body.date
     })
 
--- src/models/booking.js
+++ src/models/booking.js
@@ -6,14 +6,27 @@ const BookingSchema = mongoose.Schema({
         required: true
     },
     email: {
-        type: String
+        type: String,
+        lowercase: true,
+        trim: true,
+        minLength: 10,
+        maxLength: 10,
     },
     phone: {
-        type: String
+        type: String,
+        trim: true
+    },
+    nb_people: {
+        type: Number,
+        required: true
     },
     date: {
         type: Date,
         required: true,
         unique: true
     }
+}, {
+    timestamps: true
 })
+
+module.exports = mongoose.model('Booking', BookingSchema)
