cb729a42b4064626ae2222638389ddb90f7534e9
Messelier-Sentis <soren.messelier@gmail.com>
2022-09-22 15:54:30 +0200

    :construction: add login route

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // Import controllers
 const userController = require('./src/controllers/userController')
+const authController = require("../JungleArch-api/src/controllers/authController")
 
 // Time logger
 router.use(function timeLog (req, res, next) {
@@ -19,5 +20,8 @@ router.get('/', (req, res, next) => {
 
 router.route('/users')
     .get(userController.index)
+
+router.express.Router(), { login } = authController
+router.post("/login", authController.login, function (req, res) { });
 
 module.exports = router
