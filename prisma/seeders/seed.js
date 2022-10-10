1777487b9bf3274717f02915d8fcbfb864ae9425
Cosmeak <guillaumefine09@gmail.com>
2022-10-10 18:33:35 +0200

    :bug: FIX lint

--- prisma/seeders/seed.js
+++ prisma/seeders/seed.js
@@ -3,80 +3,80 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const words = [
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
-    {
-        civile: '',
-        impostor: ''
-    },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  },
+  {
+    civile: '',
+    impostor: ''
+  }
 ]
 
 const main = async () => {
-    console.log('Start seeding...')
-    for (const word of words) {
-        const word = await prisma.words.create({
-            data: word
-        })
-    }
-    console.log('Seeding finished.')
+  console.log('Start seeding...')
+  for (const word of words) {
+    await prisma.words.create({
+      data: word
+    })
+  }
+  console.log('Seeding finished.')
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect
+    process.exit(1)
+  })
