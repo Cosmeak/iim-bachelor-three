f0689c7bd90d1bbbd57650caff152a1e19ff4e27
Cosmeak <guillaumefine09@gmail.com>
2022-10-10 18:15:25 +0200

    :sparkles: FEAT database models

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
+const port = process.env.port || 3000
 
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
-server.listen(3000, () => {
-    console.log('listening on *:3000')
+server.listen(port, () => {
+    console.log(`🚀 Server ready at: http://localhost:${port}`)
 })
