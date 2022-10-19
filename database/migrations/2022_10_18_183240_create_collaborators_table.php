cdf9fc39a5c58d3711e27a90c864854619b2ad44
Cosmeak <guillaumefine09@gmail.com>
2022-10-19 22:11:36 +0200

    :sparkles: FEAT friends and playlists collaborators

---
+++ database/migrations/2022_10_18_183240_create_collaborators_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborators', function (Blueprint $table) {
+            $table->foreignId('playlist_id')->constrained('playlists');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborators');
+    }
+};
