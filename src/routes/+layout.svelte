8488548d3f48ade11ef8ca5716864e6420803e16
Cosmeak <guillaumefine09@gmail.com>
2022-11-17 13:22:47 +0100

    :construction: Code page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 import '../app.css';
 </script>
 
-<main class="w-screen h-screen bg-white-clay font-articula relative">
+<main class="w-screen h-screen bg-white-clay font-articula relative pt-2">
+  <div class="w-full h-auto flex justify-center items-center gap-2">
+    <h1 class="text-center text-2xl text-red-bismark font-bold">Vinci's Case</h1>
+    <img src="./logo.png" class="w-10 h-10" alt="">
+  </div>
+
   <slot />
+
+  <a href="/" class="text-red-bismark font-bold p-2 bg-beige-bis rounded-lg absolute bottom-4 left-2/4 -translate-x-2/4"><i class="fa-solid fa-house"></i></a>
 </main>
