17a55e8525254d26a8419e8a4e60ebb457db9e23
Cosmeak <guillaumefine09@gmail.com>
2022-12-16 22:09:47 +0100

    :sparkles: FEAT backend base

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+	"""Display all products store in database
+	"""
+	return render(request, 'index.html', {
+		'products': Product.objects.all()
+	})
+
+
+def create(request):
+	"""Display form to create a new product
+	"""
+	return render(request, 'form.html')
+
+
+def store(request):
+	"""Store in database the new product send by form in create function
+	"""
+	return redirect(index)
+
+
+def show(request, id:int):
+	"""Show on product based on his id
+	"""
+	return render(request, 'show.html', {
+		'product': Product.objects.get(id=id)
+	})
+
+
+def edit(request, id:int):
+	"""Show edit form for a specific product
+	"""
+	return render(request, 'form.html', {
+		'product': Product.objects.get(id=id)
+	})
+
+
+def update(request, id:int):
+	"""Update in database a specific product send with the edit form
+	"""
+	return redirect(index)
+
+
+def delete(request, id:int):
+	"""Delete a product from the database with his id
+	"""
+	Product.objects.delete(id=id)
+	return redirect(index)
