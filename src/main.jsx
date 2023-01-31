03277965ce6753b67e0592990091a8ca8d8bbeef
Cosmeak <guillaumefine09@gmail.com>
2023-01-31 10:26:58 +0100

    :sparkles: ADD api key in .env and connect api configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import pokemon from 'pokemontcgsdk'
 
-pokemon.configure({apiKey: 'xxxxxxxxxx'})
+pokemon.configure({apiKey: import.meta.env.POKEMON_TCG_KEY})
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
