543c28c28bfafd74f2c71b87a68559b89f4f2906
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-01-31 09:57:17 +0100

    React-A3.1-fcBouff: Add & import pokemontcgsdk

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import pokemon from 'pokemontcgsdk'
+
+pokemon.configure({apiKey: 'xxxxxxxxxx'})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
