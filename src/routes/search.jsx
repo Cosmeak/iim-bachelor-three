dd6b815a395219ed19a0c3a63088d3ce2b6dde6c
Steven <Steven.madi@edu.devinci.fr>
2023-01-31 14:34:45 +0100

    Change file

--- src/routes/search.jsx
+++ src/routes/search.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import pokemon from "pokemontcgsdk";
 
-const Search = () => {
-  const [card, setCard] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
+const SearchBar = () => {
+    const [card, setCard] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const results = await pokemon.card.where({ name: searchTerm });
-    setCard(results[0]);
-    console.log(results);
-  };
+    console.log(searchTerm)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const results = await pokemon.card.all({ q: 'name:' + searchTerm })
+        setCard(results[0])
+        console.log('here')
+        console.log(results.data[0].name)
+        console.log(results)
+    };
 
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} className="border rounded mr-2"/>
-        <button type="submit">Rechercher</button>
-      </form>
-      {card && <img src={card.images.small} alt={card.name} />}
-    </div>
-  );
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+                <button type="submit">Rechercher</button>
+            </form>
+            {card && <img src={card.images.small} alt={card.name} />}
+        </div>
+    );
 };
 
-export default Search;
+export default SearchBar;
