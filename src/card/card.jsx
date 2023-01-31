071051b7cec8c2a7cf5aa4fce4bb0712f37abe65
seinokoula <alexandre.charles92@gmail.com>
2023-01-31 11:53:08 +0100

    :construction: Card added

---
+++ src/card/card.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import pokemon from "pokemontcgsdk";
+
+const Card = () => {
+    const [card, setCard] = useState(null);
+
+    useEffect(() => {
+        pokemon.card.find("xy1-3").then((result) => {
+            setCard(result);
+        });
+    }, []);
+
+    return card ? <img src={card.images.small} alt={card.name} /> : null;
+};
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import pokemon from 'pokemontcgsdk'
+import Card from './card/card';
 
 pokemon.configure({apiKey: import.meta.env.POKEMON_TCG_KEY})
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Card />
   </React.StrictMode>,
 )
