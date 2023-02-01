3ab650e9f39e3af4e54e3047f5928a3920444ede
seinokoula <alexandre.charles92@gmail.com>
2023-02-01 18:02:40 +0100

    :construction: working pack opening need little tweak

--- src/pages/booster/opening.jsx
+++ src/pages/booster/opening.jsx
@@ -5,46 +5,56 @@ import AppLayout from "../../layouts/appLayout.jsx";
 import Card from "../../components/card";
 
 const opening = () => {
-    const [cards, setcards] = useState();
+    const [cards, setcards] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const params = useParams();
-    const setId = 'hgss1'
-
-    let cardsBooster = []
+    const setId = 'hgss1';
 
     const fetchBoosterCards = async () => {
-        const response = await pokemon.card.all({q: 'set.id:' + setId})
-        const randomCardId = Math.floor(Math.random() * (response.length) + 1)
-        const randomCard = response[randomCardId]
-        cardsBooster.push(randomCard)
-        setcards(cardsBooster)
+        setLoading(true);
+        const response = await pokemon.card.all({q: 'set.id:' + setId});
+        const randomCardId = Math.floor(Math.random() * (response.length) + 1);
+        const randomCard = response[randomCardId];
+        setcards((prevCards) => [...prevCards, randomCard]);
+        setLoading(false);
+    };
+    
+    {
+        cards && cards.length > 0 ? cards.map((card) => (
+            <Card key={card.id} pokemon={card} />
+        )) : (<p className="mx-auto text-center col-span-full">No card found... 😢</p>)
     }
-
     const fetchBoosterCardsRare = async () => {
-        const response = await pokemon.card.all({q: 'set.id:' + setId + ' rarity:LEGEND'})
-        const randomCardId = Math.floor(Math.random() * (response.length) + 1)
-        const randomCard = response[randomCardId]
-        cardsBooster.push(randomCard)
-    }
-
-    for (let i = 0; i < 6; i++) {
-        useEffect(() => {
-            fetchBoosterCards()
-        }, []);
-    }
+        const response = await pokemon.card.all({q: 'set.id:' + setId + ' rarity:LEGEND'});
+        const randomCardId = Math.floor(Math.random() * (response.length) + 1);
+        const randomCard = response[randomCardId];
+        setcards((prevCards) => [...prevCards, randomCard]);
+    };
 
     useEffect(() => {
-        fetchBoosterCardsRare()
+        for (let i = 0; i < 6; i++) {
+            fetchBoosterCards();
+        }
+        fetchBoosterCardsRare();
     }, []);
 
-        return (
-            <AppLayout>
-                <div className="container mx-auto mt-24 grid gap-4 grid-cols-4">
-                    {cards && cards.length > 0 ? cards.map((card) => (
+
+
+    return (
+        <AppLayout>
+            <div className="container mx-auto mt-24 grid gap-4 grid-cols-4">
+                {loading ? (
+                    <p className="mx-auto text-center col-span-full">Loading...</p>
+                ) : cards.length > 0 ? (
+                    cards.map((card) => (
                         <Card key={card.id} pokemon={card} />
-                    )) : (<p className="mx-auto text-center col-span-full">No card found... 😢</p>)}
-                </div>
-            </AppLayout>
+                    ))
+                ) : (
+                    <p className="mx-auto text-center col-span-full">No card found... 😢</p>
+                )}
+            </div>
+        </AppLayout>
     );
 };
 
