2823e04be5fe443008e7f69ed6bc25e4ab6b038e
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-02-01 09:43:31 +0100

    React-A3.1-fcBouff: Get all set

--- src/routes/singleBooster.jsx
+++ src/routes/singleBooster.jsx
@@ -5,12 +5,16 @@ import { Route, useParams } from "react-router-dom";
 
 const SingleBooster = () => {
 
-        const params = useParams();
-    
-        console.log(params); 
-    
-        return <h2> {params.boosterId}</h2>;
-    }
+    const params = useParams();
+    console.log(params['boosterId']);
+
+    pokemon.card.all({ q: 'set.id:' + params['boosterId'] }).then(result => {
+        console.log('result:')
+        console.log(result) // "Venusaur"
+    })
+
+    return <h2> {params.boosterId}</h2>;
+}
 
 
 export default SingleBooster;
