e4dc0125cb68c53437b41199835d317f87464c7d
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-02-01 15:34:07 +0100

    WIP-React-A3.1-fcBouff: Add packopening

--- src/pages/booster/opening.jsx
+++ src/pages/booster/opening.jsx
@@ -5,7 +5,48 @@ import AppLayout from "../../layouts/appLayout.jsx";
 import Card from "../../components/card";
 
 const opening = () => {
-    return (
+    const [cards, setcards] = useState();
+
+    const params = useParams();
+    const setId = 'hgss1'
+
+    let cardsBooster = []
+
+    const fetchBoosterCards = async () => {
+        const response = await pokemon.card.all({q: 'set.id:' + setId})
+        const randomCardId = Math.floor(Math.random() * (response.length) + 1)
+        const randomCard = response[randomCardId]
+        cardsBooster.push(randomCard)
+        console.log('Cards Booster:')
+        console.log(cardsBooster)
+        console.log('LENGTH: ' + cardsBooster.length)
+        if (cardsBooster.length <= 5) {
+            useEffect(() => {
+                fetchBoosterCardsRare()
+            }, []);
+        }
+    }
+
+
+    const fetchBoosterCardsRare = async () => {
+        const response = await pokemon.card.all({q: 'set.id:' + setId + ' rarity:LEGEND'})
+        const randomCardId = Math.floor(Math.random() * (response.length) + 1)
+        const randomCard = response[randomCardId]
+        cardsBooster.push(randomCard)
+    }
+
+
+    for (let i = 0; i < 6; i++) {
+        console.log(i)
+        useEffect(() => {
+            fetchBoosterCards()
+        }, []);
+    }
+
+    console.log('All booster:')
+    console.log(cardsBooster)
+
+        return (
         <AppLayout>
             <h1 className="mx-auto mt-10">Opening time</h1>
         </AppLayout>
