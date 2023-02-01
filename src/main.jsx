dd027a535fec77822de495322cff83016b83fb56
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-02-01 16:03:47 +0100

    React-A3.1-fcBouff: Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.Fragment>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.Fragment>,
 );
