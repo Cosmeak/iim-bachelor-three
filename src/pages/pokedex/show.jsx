49aa243c90f26c46fd26c85cdba9bd283293492a
Cosmeak <guillaumefine09@gmail.com>
2023-02-05 19:28:26 +0100

    :sparkles: FEAT pokemon show page

--- src/pages/pokedex/show.jsx
+++ src/pages/pokedex/show.jsx
@@ -2,7 +2,7 @@ import AppLayout from "../../layouts/appLayout.jsx";
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 
-const types = {
+const typeClasses = {
 	normal: 'px-2 py-1 rounded bg-type-normal',
 	fire: 'px-2 py-1 rounded bg-type-fire',
 	water: 'px-2 py-1 rounded bg-type-water',
@@ -23,19 +23,48 @@ const types = {
 	fairy: 'px-2 py-1 rounded bg-type-fairy',
 };
 
+const moveClasses = {
+    normal: 'px-4 py-2 rounded-lg border-2 border-type-normal',
+    fire: 'px-4 py-2 rounded-lg border-2 border-type-fire',
+    water: 'px-4 py-2 rounded-lg border-2 border-type-water',
+    electric: 'px-4 py-2 rounded-lg border-2 border-type-electric',
+    grass: 'px-4 py-2 rounded-lg border-2 border-type-grass',
+    ice: 'px-4 py-2 rounded-lg border-2 border-type-ice',
+    fighting: 'px-4 py-2 rounded-lg border-2 border-type-fighting',
+    poison: 'px-4 py-2 rounded-lg border-2 border-type-poison',
+    ground: 'px-4 py-2 rounded-lg border-2 border-type-ground',
+    flying: 'px-4 py-2 rounded-lg border-2 border-type-flying',
+    psychic: 'px-4 py-2 rounded-lg border-2 border-type-psychic',
+    bug: 'px-4 py-2 rounded-lg border-2 border-type-bug',
+    rock: 'px-4 py-2 rounded-lg border-2 border-type-rock',
+    ghost: 'px-4 py-2 rounded-lg border-2 border-type-ghost',
+    dragon: 'px-4 py-2 rounded-lg border-2 border-type-dragon',
+    dark: 'px-4 py-2 rounded-lg border-2 border-type-dark',
+    steel: 'px-4 py-2 rounded-lg border-2 border-type-steel',
+    fairy: 'px-4 py-2 rounded-lg border-2 border-type-fairy',
+};
 
 const Show = () => {
     const params = useParams();
     const [pokemon , setPokemon] = useState();
     const [abilities, setAbilities] = useState([]);
+    const [moves, setMoves] = useState([]);
     
     async function fetchPokemon() {
         let res = await fetch('https://pokeapi.co/api/v2/pokemon/' + params.pokedexId);
         res = await res.json();
         setPokemon(res);
+
+        // Get abilities
+        res.abilities.forEach(ability => {
+            fetchAbilities(ability.ability);
+        });
+
+        res.moves.forEach(move => {
+            fetchMoves(move.move);
+        });
     };
 
-    // TODO: fetch pokemon abilities to show more information about
     async function fetchAbilities(ability) {
         let res = await fetch(ability.url);
         res = await res.json();
@@ -52,19 +81,25 @@ const Show = () => {
 
     };
 
+    async function fetchMoves(move) {
+        let res = await fetch(move.url);
+        res = await res.json();
+        setMoves(prevMoves => [...prevMoves, res]);
+    };
+
     useEffect(() => {
         fetchPokemon();
     }, []);
 
     if (pokemon) console.log(pokemon)
-    if (abilities) console.log(abilities)
+    if (moves.length) console.log(moves)
 
     if(pokemon){
         return (
             <AppLayout>
                 <div className="flex flex-col lg:flex-row gap-4 mt-12">
                     <div className="w-full lg:w-4/12">
-                        <div className="bg-pokeball-white drop-shadow-lg rounded-lg p-6 relative w-full flex flex-col gap-6 items-center">
+                        <div className="bg-pokeball-white drop-shadow-lg rounded-lg p-6 w-full flex flex-col gap-6 items-center sticky top-32">
                             <img 
                                 src={pokemon.sprites.other.home.front_default} 
                                 alt=""  
@@ -77,7 +112,7 @@ const Show = () => {
                                 <div className="flex justify-center items-center uppercase text-xs gap-2 text-white mt-2">
                                     {pokemon.types.map(type => {
                                         return (
-                                            <p key={type.type.name} className={types[type.type.name]}>{type.type.name}</p>
+                                            <p key={type.type.name} className={typeClasses[type.type.name]}>{type.type.name}</p>
                                         )
                                     })}
                                 </div>
@@ -91,10 +126,10 @@ const Show = () => {
                             <div className="text-center">
                                 <h2 className="font-semibold">ABILITIES</h2>
                                 <div className="flex justify-center items-center capitalize text-xs gap-2 mt-2">
-                                    {pokemon.abilities.map(ability => {
+                                    {pokemon.abilities.map((ability, i) => {
                                         return (
                                             <p 
-                                                key={ability.ability.name}
+                                                key={i}
                                                 className="px-4 py-1 bg-pokeball-black/10 border rounded-full text-sm"
                                             >{ability.ability.name}</p>
                                         )
@@ -166,9 +201,24 @@ const Show = () => {
                         </div>
                     </div>
                     
-                    <div className="w-full lg:w-8/12 bg-pokeball-red">
-                        <p>{pokemon.name}</p>
-                        <img src={pokemon.sprites.front_default} alt="" />
+                    <div className="w-full lg:w-8/12 h-fit">
+                        <div className="bg-pokeball-white rounded-lg drop-shadow-lg p-6 w-full">
+                            <h2 className="text-3xl opacity-20 mb-2">Moves</h2>
+                            <div className="grid grid-cols-2 gap-2">
+                                {moves.map((move, i) => {
+                                    return (
+                                        <div
+                                            key={i}  
+                                            className={moveClasses[move.type.name]}
+                                        >
+                                            <h3 className="capitalize">{move.name}</h3>
+                                            <p className="font-light text-sm mt-1"><span className="underline">Description:</span> {move.flavor_text_entries.find(entry => entry.language.name == 'en').flavor_text ?? 'No text'}</p>
+                                            <p className="font-light text-sm mt-1"><span className="underline">PP:</span> {move.pp}</p>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>  
             </AppLayout>
