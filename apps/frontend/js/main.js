f66d0d83fe2929eb794c58ad318e14e3a75aca24
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-03-24 11:29:38 +0100

    iim-b3-createch: JS front return object with x, y, r, g & b

--- apps/frontend/js/main.js
+++ apps/frontend/js/main.js
@@ -9,14 +9,26 @@ function setColor() {
     body.style.backgroundColor = input.value;
     colorCode.innerHTML = input.value;
 
-    console.log(input.value)
-    console.log(hexToRgb(input.value))
+    return input.value
+
+    //console.log(input.value)
+    //console.log(hexToRgb(input.value))
 }
 
-function hexToRgb(hex) {
+function hexToRgb(hex, x, y) {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
 
-    return { r, g, b };
+    return { x, y, r, g, b };
+}
+
+function chooseColor(square) {
+    document.getElementById(square).style.backgroundColor = setColor()
+
+    let x = parseInt(square.slice(6, 7), 16)
+    let y = parseInt(square.slice(8, 9), 16)
+    let object = hexToRgb(setColor(), x, y)
+
+    console.log(object)
 }
