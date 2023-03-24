08f6e795350fb44db4bee3058a17d0992aa1af02
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-03-24 14:05:15 +0100

    iim-b3-createch: Update

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import * as http from "http";
 import cors from "cors";
+import bodyParser from "body-parser";
 import router from "./routes/router.js";
 
 const app = express();
 const server = http.createServer(app);
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(router);
 
 const port = process.env.port || 3000;
