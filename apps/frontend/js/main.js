bf1beae4ae83ef021defa9f0a50b07e7828fc48f
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-03-24 10:37:27 +0100

    iim-b3-createch: Update front

--- apps/frontend/js/main.js
+++ apps/frontend/js/main.js
@@ -1,1 +1,22 @@
 "use strict"
+const body = document.querySelector("body");
+const input = document.getElementById("colorPicker");
+const colorCode = document.getElementById("colorCode");
+setColor();
+input.addEventListener("input", setColor);
+
+function setColor() {
+    body.style.backgroundColor = input.value;
+    colorCode.innerHTML = input.value;
+
+    console.log(input.value)
+    console.log(hexToRgb(input.value))
+}
+
+function hexToRgb(hex) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    return { r, g, b };
+}
